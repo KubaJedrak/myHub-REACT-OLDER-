86df06cf1238356df0789a03fe2d8a899f09dee5
Kuba Jedrak <kuba.jedrak@gmail.com>
2021-11-21 18:15:39 +0100

    Fixed the create recipe form - fetch to be added

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,7 +1,6 @@
-import { Container, TextField, Typography, List, ListItem, ListItemText, InputLabel, Select, MenuItem, Button } from "@mui/material"
+import { Container, TextField, Typography, List, ListItem, ListItemText, InputLabel, Select, MenuItem, Button, Divider } from "@mui/material"
 import { Box } from "@mui/system"
 import { useState } from 'react'
-import AddIcon from '@mui/icons-material/Add';
 
 const CreateRecipe = () => {
 
@@ -9,36 +8,34 @@ const CreateRecipe = () => {
     name: "",
     imgURL: "",
     description: "",
-    ingredients: [    // array of objects
-      {
-        ingredName: "",
-        count: 0,
-        id: 1
-      }
-    ],
-    instructions: [   // array of strings
-
-    ]
+    ingredients: [],
+    instructions: []
   }
 
   const [name, setName] = useState("")
+  const [url, setURL] = useState("")
+  const [ingredient, setIngredient] = useState("")
   const [weight, setWeight] = useState(0)
   const [unitType, setUnit] = useState('g');
   const [ingredients, setIngredients] = useState([])
-  // const [ingredient, setIngredient] = useState({
-  //   ingredName: "",
-  //   count: 0,
-  //   id: 9999
-  // })
-
-
-
-  const changeUnit = (event) => {
-    setUnit(event.target.value);
-  };
+  const [instruction, setInstruction] = useState("")
+  const [instructions, setInstructions] = useState([])
 
   const handleName = (event) => {
-    setName(event.target.value)
+    setName(event.target.value);
+  };
+
+  const handleURL = (event) => {
+    setURL(event.target.value);
+  };
+
+  const changeUnit = (event) => {
+    if (event.target.value !== "u") setUnit(event.target.value);
+    else setUnit("");
+  };
+
+  const handleIngredient = (event) => {
+    setIngredient(event.target.value)
   }
 
   const handleWeight = (event) => {
@@ -48,98 +45,191 @@ const CreateRecipe = () => {
   const addIngredient = (event) => {
     event.preventDefault()
     setIngredients([...ingredients, {
-      ingredName: name,
+      ingredName: ingredient,
       count: `${weight}${unitType}`,
       id: Math.round(Math.random() * 1000)
     }])
-    console.log(ingredients)
+    event.target.reset()
+  }
+
+  const addInstruction = (event) => {
+    setInstruction(event.target.value)
+  }
+
+  const addInstructions = (event) => {
+    event.preventDefault()
+    setInstructions([...instructions, instruction])
+    event.target.reset()
+  }
+
+  const submitRecipe = () => {
+
+    newRecipe = {
+      name: name,
+      imgURL: url,
+      description: "",
+      ingredients: ingredients,
+      instructions: instructions
+    }
+    // fetch("http://localhost:8000/recipes", {
+    //   method: "PUT",
+    //   body: newRecipe
+    // })
+
+    console.log(newRecipe)
+
+    setName("")
+    setWeight(0)
+    setUnit('g')
+    setIngredients([])
+    setInstruction("")
+    setInstructions([])
+
+    newRecipe = {
+      name: "",
+      imgURL: "",
+      description: "",
+      ingredients: [],
+      instructions: []
+    }
+
+    console.log(newRecipe)
+
   }
 
   return (
-    <Container sx={{padding: "20px 0px", minHeight: "94vh"}}>
+    <Container sx={{padding: "20px 0px", minHeight: "94vh", marginBottom: "70px"}}>
       <Typography variant="h5" margin="0px 0px 20px">
         Create a new recipe
       </Typography>
 
+      {/* Main Info Form */}
       <Box 
         component="form"
         noValidate
         autoComplete="off"
         sx={{
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
           flexWrap: "wrap",
           justifyContent: "center"
         }}
       >
-
-        <TextField id="newRecipe-name" label="Recipe Name" variant="outlined" sx={{
+        <Typography variant="body1">Add Name and Picture</Typography>
+        <TextField id="newRecipe-name" label="Recipe Name" variant="outlined" onChange={handleName} sx={{
           width: "50vw",
-          margin: "20px auto",
+          margin: "10px auto",
 
         }} required />
-        <TextField id="newRecipe-img-url" label="Recipe Image Address" variant="outlined" sx={{
+        <TextField id="newRecipe-img-url" label="Recipe Image Address" onChange={handleURL} variant="outlined" sx={{
           width: "50vw",
-          margin: "20px auto"
-        }} required />        
-        {/* {(ingredientList.length === 0) && <Typography sx={{margin: "20px 0px", border: "1px solid #9EADBD", borderRadius: "4px" }}>Start adding ingredients below</Typography>} */}
+          margin: "10px auto"
+        }} required />             
+      </Box>
+      <Divider sx={{width: "80%"}} />
 
+      <Box 
+        sx={{
+            display: "block", 
+            width: "60%", 
+            margin: "20px 0px", 
+            padding: "0px 8px" , 
+            border: "1px solid #9EADBD", 
+            borderRadius: "4px", 
+            textAlign: "left", 
+            boxSizing: "border-box",
+            backgroundColor: "#fff"
+          }}
+        >
+          {(ingredients.length === 0) && <Typography sx={{margin: "20px 0px"}}>Start adding ingredients below</Typography>}
+          {ingredients.map( ingredient => (
 
-      
+            <Box key={ingredient.id}>
+              <Typography>{ingredient.ingredName} ({ingredient.count})</Typography>
+              <Divider sx={{width: "100%"}} />
+            </Box>
+          ))}
       </Box>
   
-      <Box sx={{alignSelf: "center"}} component="form" >
-
-        <TextField id="newRecipe-ingredient" onChange={handleName} label="Add one ingredient at a time" variant="outlined" sx={{width: "50vw", margin: "20px auto"}} required />    
-        <TextField id="newRecipe-ingredient" onChange={handleWeight} label="How much?" variant="outlined" sx={{width: "50vw", margin: "20px auto"}} required />    
-        <InputLabel id="newRecipe-ingredient-unitCount-label">Units</InputLabel>
+      {/* Ingredients Form */}
+      <Box sx={{alignSelf: "center"}} component="form" onSubmit={addIngredient} >
+        <Typography variant="body1">Add ingredients (one at a time)</Typography>
+        <TextField id="newRecipe-ingredient" onChange={handleIngredient} label="Add one ingredient at a time" variant="outlined" sx={{width: "50vw", margin: "10px auto"}} required />    
+        <TextField id="newRecipe-ingredient" onChange={handleWeight} label="How much?" variant="outlined" sx={{width: "50vw", margin: "10px auto"}} required />    
         <Select
           labelId="newRecipe-ingredient-unitCount-label"
           id="newRecipe-ingredient-unitCount"
-          label="Unit Type"
+          label="Units"
           defaultValue="g"
           value={unitType}
           onChange={changeUnit}
-          sx={{width: "50vw"}}
+          sx={{width: "50vw", margin: "10px auto"}}
         >
           <MenuItem value="g">Grams</MenuItem>
           <MenuItem value="ml">Milliliters</MenuItem>
-          <MenuItem value="units">Item Count</MenuItem>
+          <MenuItem value="u">Item Count</MenuItem>
         </Select>
         <Button 
-          variant="contained" 
-          color="secondary" 
+          variant="outlined" 
+          color="primary" 
           sx={{alignSelf: "center", justifySelf: "center", margin: "20px", display: "block"}}
-          onClick={addIngredient}
           type="submit"
         >
         Add Ingredient
         </Button>
-
-        
-
-        <Box className="listBox">
-          <List>
-            {ingredients.map( ingredient => (
-              <ListItem key={ingredient.id}>
-                <ListItemText primary={ingredient.name} />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
       </Box>
 
+      <Divider sx={{width: "80%"}} />
 
+      <Box sx={{
+            display: "block", 
+            width: "60%", 
+            margin: "20px 0px", 
+            padding: "0px 8px" , 
+            border: "1px solid #9EADBD", 
+            borderRadius: "4px", 
+            textAlign: "left", 
+            boxSizing: "border-box",
+            backgroundColor: "#fff"
+          }}
+        >
+          {(instructions.length === 0) && <Typography sx={{margin: "20px 0px"}}>Start adding instructions below</Typography>}
+          
+          {instructions.map( (instruction, index) => (
+            <Box key={index}>
+              <Typography >{instruction}</Typography>
+              <Divider sx={{width: "100%"}} />
+            </Box>
+          ))}
+      </Box>
 
+      {/* Instructions Form */}
+      <Box sx={{alignSelf: "center"}} component="form" onSubmit={addInstructions}>
+        <Typography variant="body1">Add instructions (one at a time)</Typography>
+        <TextField id="newRecipe-instructions" label="What do I do?!" onChange={addInstruction} variant="outlined" sx={{width: "50vw", margin: "10px auto"}} required />    
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          sx={{alignSelf: "center", justifySelf: "center", margin: "20px", display: "block"}}
+          type="submit"
+        >
+        Add Instruction
+        </Button>
+      </Box>
 
+      <Button 
+        variant="contained" 
+        color="secondary" 
+        onClick={submitRecipe}
+        sx={{
+          width: "60%",
+          font: "24px"
+        }}
+      >
+        Add recipe!
+      </Button>
     </Container>
-
-
-
-
-
   )
-
 }
 
 export default CreateRecipe
